b1c2cea11b2ce5f14c1d15fa6f4941fa88b934fc
dmad103 <104371134+dmad103@users.noreply.github.com>
2023-04-03 19:19:44 -0400

    Add files via upload

---
+++ leagueInfoObj.js
@@ -0,0 +1,8 @@
+const leagueInfo = {
+	_id: "640537b7187fd8853884205b",
+	leagueID: "39",
+	leagueName: "Premier League",
+	leagueCountry: "England",
+	leagueLogo: "https://media.api-sports.io/football/leagues/39.png",
+	__v: 0,
+};
